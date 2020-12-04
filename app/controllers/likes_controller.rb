f78d4f47569eb9abba47e893e37cd4050ce5992c
github hayanospl <hayano3255@gmail.com>
2020-12-04 12:45:47 +0900

    投稿いいね機能の実装(likes_controllerの修正)

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,12 @@
 class LikesController < ApplicationController
   def create
-    @post = Post.find(params[:post_id])
     @like = current_user.likes.create(post_id: params[:post_id])
-    redirect_to post_path(@post.id)
+    redirect_to post_path(params[:post_id])
   end
 
   def destroy
-    @post = Post.find(params[:post_id])
     @like = Like.find_by(post_id: params[:post_id], user_id: current_user.id)
     @like.destroy
-    redirect_to post_path(@post.id)
+    redirect_to post_path(params[:post_id])
   end
 end
