afc23bd03d704d64e876d0ad7cc2950221a11c84
github hayanospl <hayano3255@gmail.com>
2020-12-06 15:57:22 +0900

    AddImageToPostファイルの削除、修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_06_012013) do
+ActiveRecord::Schema.define(version: 2020_12_04_082837) do
 
   create_table "comment_likes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -44,9 +44,9 @@ ActiveRecord::Schema.define(version: 2020_12_06_012013) do
     t.string "title"
     t.text "content"
     t.bigint "user_id", null: false
+    t.string "image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "image"
     t.index ["user_id", "created_at"], name: "index_posts_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
