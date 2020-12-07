995cc58ceec05114f8131db832f18ac8ac097260
github hayanospl <hayano3255@gmail.com>
2020-12-07 11:06:22 +0900

    コメントに画像動画添付機能追加

--- db/schema.rb
+++ db/schema.rb
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_12_04_082837) do
     t.string "content"
     t.bigint "user_id", null: false
     t.bigint "post_id", null: false
+    t.string "image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
