9137fd853b71dbcdc781c8d21d6c7270108c6453
github hayanospl <hayano3255@gmail.com>
2020-12-10 17:01:32 +0900

    タグの実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -62,6 +62,6 @@ class PostsController < ApplicationController
 
   private
   def post_params
-    params.require(:post).permit(:title, :content, :image)
+    params.require(:post).permit(:title, :content, :image, :tag_list)
   end
 end
