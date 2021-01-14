4caddace1e8b0f08e8bc243b2623856bf19323ce
github hayanospl <hayano3255@gmail.com>
2021-01-14 16:53:43 +0900

    heroku デプロイ6

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,9 +6,8 @@ window.$ = $;
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
-require("jquery3")
-require("jquery_ujs")
-require("popper")
+require("jquery")
+//require("popper")
 require("bootstrap")
 require("channels")
 
