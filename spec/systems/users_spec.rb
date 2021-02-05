a3e20976f12037381f2a91b1fe39d2d93264f5d4
github hayanospl <hayano3255@gmail.com>
2021-02-05 11:39:56 +0900

    system spec 新規登録画面のレビュー修正

--- spec/systems/users_spec.rb
+++ spec/systems/users_spec.rb
@@ -10,11 +10,13 @@ RSpec.describe "Users create", type: :system do
 
       context 'フォームの入力値が正常な時' do
         it 'ユーザーの新規作成が成功' do
-          fill_in 'Name', with: 'new_commer'
-          fill_in 'Eメール', with: 'test@example.com'
-          fill_in 'パスワード', with: 'password'
-          fill_in 'パスワード（確認用）', with: 'password'
-          click_button '新規登録'
+          within("form#new_user") do
+            fill_in 'Name', with: 'new_commer'
+            fill_in 'Eメール', with: 'test@example.com'
+            fill_in 'パスワード', with: 'password'
+            fill_in 'パスワード（確認用）', with: 'password'
+            click_button '新規登録'
+          end
           expect(current_path).to eq root_path
           expect(page).to have_content '新規登録が完了しました'
         end
@@ -22,11 +24,13 @@ RSpec.describe "Users create", type: :system do
 
       context '名前が未記入の時' do
         it 'ユーザーの新規作成が失敗' do
-          fill_in 'Name', with: nil
-          fill_in 'Eメール', with: 'test@example.com'
-          fill_in 'パスワード', with: 'password'
-          fill_in 'パスワード（確認用）', with: 'password'
-          click_button '新規登録'
+          within("form#new_user") do
+            fill_in 'Name', with: nil
+            fill_in 'Eメール', with: 'test@example.com'
+            fill_in 'パスワード', with: 'password'
+            fill_in 'パスワード（確認用）', with: 'password'
+            click_button '新規登録'
+          end
           expect(current_path).to eq '/users'
           expect(page).to have_content 'Nameを入力してください'
         end
@@ -34,11 +38,13 @@ RSpec.describe "Users create", type: :system do
 
       context 'メールアドレスが未記入の時' do
         it 'ユーザーの新規作成が失敗' do
-          fill_in 'Name', with: 'new_commer'
-          fill_in 'Eメール', with: nil
-          fill_in 'パスワード', with: 'password'
-          fill_in 'パスワード（確認用）', with: 'password'
-          click_button '新規登録'
+          within("form#new_user") do
+            fill_in 'Name', with: 'new_commer'
+            fill_in 'Eメール', with: nil
+            fill_in 'パスワード', with: 'password'
+            fill_in 'パスワード（確認用）', with: 'password'
+            click_button '新規登録'
+          end
           expect(current_path).to eq '/users'
           expect(page).to have_content 'Eメールを入力してください'
         end
@@ -46,11 +52,13 @@ RSpec.describe "Users create", type: :system do
 
       context '登録済みメールアドレスの時' do
         it 'ユーザーの新規作成が失敗' do
-          fill_in 'Name', with: 'new_commer'
-          fill_in 'Eメール', with: user.email
-          fill_in 'パスワード', with: 'password'
-          fill_in 'パスワード（確認用）', with: 'password'
-          click_button '新規登録'
+          within("form#new_user") do
+            fill_in 'Name', with: 'new_commer'
+            fill_in 'Eメール', with: user.email
+            fill_in 'パスワード', with: 'password'
+            fill_in 'パスワード（確認用）', with: 'password'
+            click_button '新規登録'
+          end
           expect(current_path).to eq '/users'
           expect(page).to have_content 'Eメールはすでに存在します'
         end
@@ -58,11 +66,13 @@ RSpec.describe "Users create", type: :system do
 
       context 'パスワード（確認用）が未記入の時', js: true do
         it 'ユーザーの新規作成が失敗' do
-          fill_in 'Name', with: 'new_commer'
-          fill_in 'Eメール', with: 'test@example.com'
-          fill_in 'パスワード', with: 'password'
-          fill_in 'パスワード（確認用）', with: nil
-          click_button '新規登録'
+          within("form#new_user") do
+            fill_in 'Name', with: 'new_commer'
+            fill_in 'Eメール', with: 'test@example.com'
+            fill_in 'パスワード', with: 'password'
+            fill_in 'パスワード（確認用）', with: nil
+            click_button '新規登録'
+          end
           expect(current_path).to eq '/users'
           expect(page).to have_content 'パスワード（確認用）とパスワードの入力が一致しません'
         end
